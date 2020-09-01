4c815eb5f78a636e834fbbe1fd1079b55983b8ff
CatWallin <cwallin@student.rccd.edu>
2020-08-31 17:33:18 -0700

    Signed-off-by: CatWallin <cwallin@student.rccd.edu>

---
+++ Homework/IntroLab/IntroLab/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * File:   main.cpp
+ * Author: Catherine Wallin
+ * Created on August 27, 2020, 12:09 PM
+ * Purpose: Intro Lab
+ */
+
+//System Libraries
+#include <iostream> //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //set the random number seed here
+    
+    //Declare all variables for this function
+    
+    //Initialize all known variables
+    
+    //Process Inputs to Outputs -> mapping process
+    //Maps known values to the unknown objects
+    
+    //Display Inputs/Outputs 
+    cout << "Hello World!" << endl;
+    
+    //Clean up the code, close files, deallocate memory, etc...
+    //Exit
+    return 0;
+}
+ //Function Implementation
